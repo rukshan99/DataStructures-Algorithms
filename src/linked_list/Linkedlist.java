8753c3ad47c99b3125d2286d47a71037ef5353c8
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2021-03-09 13:27:46 +0530

    Linked List (Java)

---
+++ src/linked_list/Linkedlist.java
@@ -0,0 +1,21 @@
+package linked_list;
+
+import java.util.*;
+
+public class Linkedlist {
+
+	public static void main(String[] args) {
+		
+	
+		  LinkedList<String> list=new LinkedList<>();  
+		  list.add("Rukshan");  
+		  list.add("Nimal");  
+		  list.add("Kamal");  
+		  list.add("Sunil");  
+		  
+		  Iterator<String> itr=list.iterator();  
+		  while(itr.hasNext()){  
+		   System.out.println(itr.next());  
+		  }
+	}
+}
